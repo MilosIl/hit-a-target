b1fa17fa2574ee18508968146dc2ca9b30beec34
Milos <milos.ilic20@hotmail.com>
2023-10-23 19:23:41 +0200

    title updated

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import { useState, useEffect } from "react";
 function App() {
   const [target, setTarget] = useState<boolean[]>(new Array(9).fill(false));
   const [score, setScore] = useState<number>(0);
-  const [mode, setMode] = useState(1000);
+  const [maxScore, setMaxScore] = useState<number>(0);
+  const [mode, setMode] = useState<number>(1000);
   const [isGameOver, setIsGameOver] = useState<boolean>(false);
 
   function setTargetVisibility(index: number, isVisible: boolean) {
@@ -14,13 +15,14 @@ function App() {
       return newTarget;
     });
   }
-
-  function handleStopGame() {
-    setIsGameOver(!isGameOver);
+  function StartGame() {
+    setIsGameOver(true);
+    setMaxScore(0);
     if (isGameOver) {
       setScore(0);
     }
   }
+
   function changeMode(speed: number, size: string) {
     document.documentElement.style.setProperty("--width-target", size);
     setMode(speed);
@@ -36,10 +38,11 @@ function App() {
   useEffect(() => {
     const interval = setInterval(() => {
       let randomIndex = Math.floor(Math.random() * target.length);
-      if (isGameOver) {
+      if (!isGameOver) {
         return (randomIndex = 0);
       }
       setTargetVisibility(randomIndex, true);
+      setMaxScore(maxScore + 1);
       setTimeout(() => {
         setTargetVisibility(randomIndex, false);
       }, mode);
@@ -47,21 +50,29 @@ function App() {
     return () => {
       clearInterval(interval);
     };
-  }, [target, mode, isGameOver]);
+  }, [target, mode, isGameOver, maxScore]);
 
   return (
     <div className="container">
+      <h1>Improve your reflexes and mouse control.</h1>
       <div className="score">
-        <button onClick={() => handleStopGame()}>
-          {!isGameOver ? "stop game" : "reset game"}
-        </button>
-        {isGameOver ? <p className="title">Score: {score}</p> : ""}
+        {isGameOver ? (
+          <button onClick={() => setIsGameOver(!isGameOver)}>
+            {isGameOver ? "stop game" : "reset game"}
+          </button>
+        ) : (
+          <button onClick={StartGame}>start game</button>
+        )}
+
+        <p className="title">
+          Score: {score}/{maxScore}
+        </p>
       </div>
       <div className="difficulty">
         Set difficulty:
         <button onClick={() => changeMode(800, "8em")}>easy</button>
         <button onClick={() => changeMode(600, "5em")}>medium</button>
-        <button onClick={() => changeMode(350, "2.5em")}>hard</button>
+        <button onClick={() => changeMode(400, "3em")}>hard</button>
       </div>
       <div className="target-container">
         {target.map((isTarget, index) => {
